2578d493b66d79e8456b71da5e860e5c76b89608
Adrian Pekarek <kzadorlr@gmail.com>
2017-11-29 10:44:26 -0300

    Firt commit

---
+++ WaterlooBankObjC/Pods/Target Support Files/GZIP/GZIP-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "GZIP.h"
+#import "NSData+GZIP.h"
+
+FOUNDATION_EXPORT double GZIPVersionNumber;
+FOUNDATION_EXPORT const unsigned char GZIPVersionString[];
+
---
+++ iProov.framework/Headers/iProov-umbrella.h
@@ -0,0 +1,11 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "Encoder.h"
+#import "FlashingFilter.h"
+#import "iProov.h"
+
+FOUNDATION_EXPORT double iProovVersionNumber;
+FOUNDATION_EXPORT const unsigned char iProovVersionString[];
+
